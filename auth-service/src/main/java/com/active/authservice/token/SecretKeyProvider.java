a3e2fe0b692e9814563cdc0876a8ebe36f761622
Alexander Asenov <alexspx@hotmail.com>
2025-05-18 23:21:56 +0300

    fix secret

--- auth-service/src/main/java/com/active/authservice/token/SecretKeyProvider.java
+++ auth-service/src/main/java/com/active/authservice/token/SecretKeyProvider.java
@@ -8,8 +8,8 @@ import javax.crypto.SecretKey;
 
 @Component
 public class SecretKeyProvider {
-//    @Value("${jwt.secret}")
-    private String secret = "9a4f2c8d3b7a1e6f45c8a0b3f267d8b1d4e6f3c8a9d2b5f8e3a9c8b5f6v8a3d9";
+    @Value("${jwt.secret}")
+    private String secret;
 
     public SecretKey getSecretKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
